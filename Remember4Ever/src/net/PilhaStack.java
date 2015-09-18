6d42e9ea4b884b3f145d9a9d31107a759b6ba9af
Nitrogen-Dioxide <nitrogen.dioxide.99@gmail.com>
2015-09-18 11:19:26 -0300

    Better notes for stack

--- Remember4Ever/src/net/PilhaStack.java
+++ Remember4Ever/src/net/PilhaStack.java
@@ -1,5 +1,7 @@
 package net;
 
+import java.util.Scanner;
+
 /**
 * Fatorial de N utilizando o conceito de pilha
 *@autor Tarcnux
@@ -13,33 +15,35 @@ package net;
 */
 
 import java.util.Stack;
-import java.util.Scanner;
 
 public class PilhaStack {
-	
-	public static void main (String[] args)
-	  {
-	    Scanner scan = new Scanner ( System.in );
-	    int n=1;
-	    System.out.print("\nDigite um numero: ");   
-	    n = scan.nextInt();
-	    Fatorialx(n);
-	      
-	  }
 
-	  public static void Fatorialx( int N )
-	  {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = 1;
+		System.out.print("\nDigite um numero: ");
+		n = scan.nextInt();
+		Fatorialx(n);
 		
-	        Stack <Integer> pilha = new Stack<Integer>();
-		int fatorial = 1, aux = N;
-		
-		//Empilha
-		while(aux > 1)
-			pilha.push(aux--); //Empilha e depois decrementa
-		
-		while(!pilha.empty()) //Enquanto há elementos na pilha
-			fatorial *= pilha.pop(); //Desempilha e calcula o Fatorial
-			
-		System.out.println("O fatorial de " + N + " é: " + fatorial);
-	  }
+		scan.close();
+
+	}
+
+	public static void Fatorialx(long N) {
+
+		Stack<Long> pilha = new Stack<Long>();
+		long fatorial = 1, aux = N;
+
+		// Empilha
+		while (aux > 1)
+			pilha.push(aux--); // Empilha e depois decrementa
+
+		System.out.println("Foram incrementados " + pilha.size() + " na pilha\nO numero " + pilha.peek()
+				+ " e o topo da fila e o " + pilha.elementAt(0) + " e a base da pilha.");
+
+		while (!pilha.empty()) // Enquanto hï¿½ elementos na pilha
+			fatorial *= pilha.pop(); // Desempilha e calcula o Fatorial
+
+		System.out.println("O fatorial de " + N + " ï¿½: " + fatorial);
+	}
 }
