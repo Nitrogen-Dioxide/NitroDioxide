9ee0963fe440a222461df733628765717f40ace4
Nitrogen-Dioxide <nitrogen.dioxide.99@gmail.com>
2015-09-17 23:07:50 -0300

    Added continue in practice

---
+++ Remember4Ever/src/net/ContinueDef.java
@@ -0,0 +1,20 @@
+package net;
+
+public class ContinueDef {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i <= 10; i++){
+			if (i % 2 == 0)
+				continue;
+			System.out.println("---\nNumero impar " + i );
+		}
+		
+		for (int i = 0; i <= 10; i++){
+			if (i % 2 != 0)
+				continue;
+			System.out.println("---\nNumero par " + i );
+		}
+	}
+
+}
